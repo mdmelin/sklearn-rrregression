c953c18f925061e64673c5d5d454ac524cfa4b70
Max Melin <mmelin@g.ucla.edu>
2024-04-04 16:58:13 -0700

    fit reduced rank regression

--- examples/plot_template.py
+++ examples/plot_template.py
@@ -9,10 +9,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 from skltemplate import ReducedRankRegression
 
-X = np.arange(100).reshape(100, 1)
-y = np.zeros((100, ))
-estimator = ReducedRankRegression()
+X = np.arange(1000).reshape(100, 10)
+y = np.zeros((100, 10))
+
+X = np.random.uniform(0,1, size=(100,40))
+y = np.random.normal(size=(100,10))
+
+estimator = ReducedRankRegression(rank=2)
 estimator.fit(X, y)
 plt.plot(y)
 plt.plot(estimator.predict(X), alpha=.8, linestyle = '--')
 plt.show()
+print(estimator.get_params())
